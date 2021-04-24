ed3fbeb01065a4f8ed072b0975833947a24cab72
unknown <thana.najem3@gmail.com>
2021-04-24 10:24:04 +0300

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux' 
+import { connect } from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 class App extends Component {
 
   render() {
     return (
-      <div class="products">
-        {this.props.products.map((product) => (
-          <div class="card" style="width: 18rem;">
-          <img src={`/images/${product.image}`} class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">{product.productName}</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
+      <div class="container">
+        <div class="row">{this.props.products.map((product) => (
+          <div class="col-xs-12 col-md-4 col-lg-3">
+            <div class="card" style={{ width: 18 + 'rem' }}>
+              <img src={`/images/${product.image}`} class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">{product.productName}</h5>
+                <p class="card-text">{product.description}</p>
+                <a href="#" class="btn btn-primary">Details</a>
+              </div>
+            </div>
+
+          </div> ))}
         </div>
-        ))}
-        
+     
+
 
       </div>
     )
   }
-} 
+}
 function mapStateToProps(state) {
   console.log(state)
   return {
