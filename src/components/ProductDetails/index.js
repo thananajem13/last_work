1011a1fea1cbb552cfcb7ed0e48b6c2494f627e1
unknown <thana.najem3@gmail.com>
2021-04-25 05:46:11 +0300

    --- Auto-Git Commit ---

--- src/components/ProductDetails/index.js
+++ src/components/ProductDetails/index.js
@@ -9,57 +9,58 @@ import {
     Link,
     useParams
 } from "react-router-dom";
+import Navbar from '../Navbar/Navbar';
 
-const Index = ({products}) => {
-    // id = null;
-    // product = null
-    // componentDidMount() {
-    //    id = this.props.match.params.id; 
-    // }  
+const Index = ({ products }) => {
     let { id } = useParams();
-    console.log(id); 
-    console.log(products); 
     const product_details_by_id = products.find(product =>
         product.id === id);
-    console.log(product_details_by_id);
     return (
+        <div>
+            <Navbar />
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 offset-md-2">
+                        <div className="row" id="gradient">
+                            <div className="col-md-4">
+                                <img src={`/images/${product_details_by_id.image}`} className="img-fluid" alt="product image" />
+                            </div>
+                            <div className="col-md-8" id="overview">
+                                <div className="row">
+                                    <div className="col-xs-6 col-md-6">
+                                        <ul className="pb-product-details-ul">
+                                            <li><span className="fa fa-product-hunt">&nbsp;{`ProductName: ${product_details_by_id.ProductName} `}</span></li>
+                                            <li><span className="fa fa-microchip">&nbsp;{`CPU: ${product_details_by_id.cpu} `}</span></li>
+                                            <li><span className="fa fa-laptop">&nbsp;{`Screen: ${product_details_by_id.screen} `}</span></li>
+                                            <li><span className="fa fa-memory">&nbsp;{`RAM: ${product_details_by_id.ram} `}</span></li>
+                                            <li><span className="fa fa-hdd">&nbsp;{`Storage: ${product_details_by_id.storage} `} </span></li>
+                                            <li><span className="fa fa-dollar-sign">&nbsp;{`Price: ${product_details_by_id.price}$ `} </span></li>
+                                            <li><span className="fa fa-audio-description">&nbsp;{`Description: ${product_details_by_id.description} `} </span></li>
+                                        </ul>
+                                    </div>
+                                    <div className="col-xs-6 col-md-6">
+                                        <div className="row">
+                                            <div className="col-xs-10 col-md-10 text-xs-center" id="hits">
+                                                <span className="fa fa-sort-amount-up">&nbsp;</span>
+                                                <input type="number" min="1" className="d-block" />
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col-xs-10 col-md-10 text-xs-center " id="fan">
+                                                <button className="btn btn-danger form-control d-block"><span className="fa fa-cart-plus">&nbsp;</span>Add to cart</button>
+                                            </div>
+                                        </div>
+                                    </div>
 
-        <div className="container">
-            {
-            }
-            <div className="row">
-                <div className="col-md-8 offset-md-2">
-                    <div className="row" id="gradient">
-                        <div className="col-md-4">
-                            <img src={`/images/${product_details_by_id.image}`} className="img-fluid" alt="product image" />
+
+                                </div>
+                            </div>
                         </div>
-                        <div className="col-md-8" id="overview">
-                            <div className="row">
-                                <div className="col-xs-6 col-md-6">
-                                    <ul className="pb-product-details-ul">
-                                        <li><span className="fa fa-product-hunt">&nbsp;{`ProductName: ${product_details_by_id.ProductName} `}</span></li>
-                                        <li><span className="fa fa-microchip">&nbsp;{`CPU: ${product_details_by_id.cpu} `}</span></li>
-                                        <li><span className="fa fa-laptop">&nbsp;{`Screen: ${product_details_by_id.screen} `}</span></li>
-                                        <li><span className="fa fa-memory">&nbsp;{`RAM: ${product_details_by_id.ram} `}</span></li>
-                                        <li><span className="fa fa-hdd">&nbsp;{`Storage: ${product_details_by_id.storage} `} </span></li>
-                                        <li><span className="fa fa-dollar-sign">&nbsp;{`Price: ${product_details_by_id.price} `} </span></li>
-                                        <li><span className="fa fa-audio-description">&nbsp;{`Description: ${product_details_by_id.description} `} </span></li>
-                                    </ul>
-                                </div>
-                                <div className="col-xs-3 col-md-3 text-xs-center" id="hits">
-                                    <span className="fa fa-sort-amount-up">&nbsp;</span>
-                                    
-                                    <input type="number"  />
-                                </div>
-                                <div className="col-xs-3 col-md-3 text-xs-center" id="fan">
-                                    <button className="btn btn-danger"><span className="fa fa-cart-plus">&nbsp;</span>Add to cart</button>
-                                </div>
-                            </div>
-                            </div>
                     </div>
-                     </div>
+                </div>
             </div>
         </div>
+
 
     )
 }
